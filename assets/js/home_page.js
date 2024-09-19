53cdc6650f074aa01cf8dbe16f6d890b6b69cf77
Alex Carqueja <up201705049@fe.up.pt>
2024-09-19 22:41:40 +0000

    desktop homepage good enough

--- assets/js/home_page.js
+++ assets/js/home_page.js
@@ -1,6 +1,6 @@
 // TODO: (I should find a better way to do this
 window.addEventListener('load', function() {
     const carousel = FlowbiteInstances.getInstance('Carousel', 'background-slideshow');
-    carousel._intervalDuration = 6000
+    carousel._intervalDuration = 8000
     console.log(carousel)
 })
