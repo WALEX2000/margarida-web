d88b6ad51b86060677968b1a871ab61c22605e2b
Alex Carqueja <up201705049@fe.up.pt>
2024-09-19 17:28:59 +0000

    Background slider operational

---
+++ assets/js/home_page.js
@@ -0,0 +1,6 @@
+// TODO: (I should find a better way to do this
+window.addEventListener('load', function() {
+    const carousel = FlowbiteInstances.getInstance('Carousel', 'background-slideshow');
+    carousel._intervalDuration = 6000
+    console.log(carousel)
+})
--- content/about.md
+++ content/about.md
@@ -1,5 +1,5 @@
 ---
-title: "About"
+title: "About Me"
 ---
 Here is some content.
 Super simple to write
