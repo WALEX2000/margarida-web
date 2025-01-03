db33c7927080bb0d2ea5d1a0381536c517d0d2f6
Alex Carqueja <up201705049@fe.up.pt>
2025-01-03 17:27:41 +0000

    small changes to preview

---
+++ public/admin/landscape_image_setter.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => {
+        console.log(img)
+        img.onload = function() {
+            if (img.naturalWidth > img.naturalHeight) {
+                img.classList.add('landscape');
+            }
+        };
+        // For cached images that are already loaded
+        if (img.complete) {
+            img.onload();
+        }
+    });
+});
